d3b458c2dcc8dc5ffc84f5ea023d8fa3dd332be9
manpreetkaur292006-design <manpreetkaur292006@gmail.com>
2026-04-10 19:24:01 +0530

    added all the files

---
+++ Basic-Calculator-React-JS/src/Components/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Display = () => {
+  return (
+    <>
+        <input type="text" id="display" class="calculator-display" readonly />
+    </>
+  )
+}
+
+export default Display
